18d5a25ae642067167a87f2e562481794c30980f
cosmo <hans.corsmit@telenet.be>
2020-09-23 19:29:27 +0200

    Spring add test base

---
+++ src/main/java/guru/springframework/hcopetclinic/HcoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.hcopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcoPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcoPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/hcopetclinic/HcoPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.hcopetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcoPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
