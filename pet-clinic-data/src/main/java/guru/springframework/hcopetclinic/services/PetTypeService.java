0fcdf5de4b46cbf1d85d256e3cdd910ba059b0dc
cosmo <hans.corsmit@telenet.be>
2020-10-04 19:29:09 +0200

    implement pet type map service closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/hcopetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.hcopetclinic.services;
+
+import guru.springframework.hcopetclinic.model.PetType;
+import guru.springframework.hcopetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
