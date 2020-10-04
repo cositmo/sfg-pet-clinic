d2c9c8fd9eaafb76f89c74ae88677f2797f7adc1
cosmo <hans.corsmit@telenet.be>
2020-10-04 19:17:14 +0200

    add speciality and add to vet closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/hcopetclinic/model/Speciality.java
@@ -0,0 +1,15 @@
+package guru.springframework.hcopetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+
+}
