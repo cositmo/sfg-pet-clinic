c10fd33d4f85ff254ed8f9ad7d0a9f7ccdf4dfb0
cosmo <hans.corsmit@telenet.be>
2020-10-05 09:24:02 +0200

    implement SpecialityService closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/hcopetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package guru.springframework.hcopetclinic.services;
+
+import guru.springframework.hcopetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/hcopetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package guru.springframework.hcopetclinic.services.map;
+
+import guru.springframework.hcopetclinic.model.Speciality;
+import guru.springframework.hcopetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap  extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+super.deletebyId(aLong);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
