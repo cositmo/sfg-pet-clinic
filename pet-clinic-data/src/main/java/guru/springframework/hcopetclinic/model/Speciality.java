8343dc9bcfbbc644ff3585786424dc64c603e895
cosmo <hans.corsmit@telenet.be>
2020-10-07 14:47:02 +0200

    implement vets as jpa entity closes #37

--- pet-clinic-data/src/main/java/guru/springframework/hcopetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/hcopetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package guru.springframework.hcopetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     public String getDescription() {
         return description;
     }
